91cff94e56dbfd2ecc33ec650e0b7951b28716c6
Martin Dawson <martino1995@blueyonder.co.uk>
2017-09-11 14:47:36 +0100

    pruned react-jplayer-utils

--- src/index.js
+++ src/index.js
@@ -1,29 +1,21 @@
-import connect from './connect';
 import connectWithId from './connectWithId';
 import connectWithIndex from './connectWithIndex';
-import initialState from './initialState';
 import formatArtistAndTitle from './formatArtistAndTitle';
 import classes from './classes';
-import KeyControl from './components/keyControl';
 import toPercentage from './toPercentage';
 import toRelativePercentage from './toRelativePercentage';
-import traverseParentsUntilClassName from './traverseParentsUntilClassName';
 import getElementOffset from './getElementOffset';
 import limitValue from './limitValue';
 import canSetVolume from './canSetVolume';
 import convertTime from './convertTime';
 
 export {
-  connect,
   connectWithId,
   connectWithIndex,
-  initialState,
   formatArtistAndTitle,
   classes,
-  KeyControl,
   toPercentage,
   toRelativePercentage,
-  traverseParentsUntilClassName,
   getElementOffset,
   limitValue,
   canSetVolume,
